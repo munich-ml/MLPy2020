4a1e1dd6a6a52e8b4c52885faedf1e8b3aebbcea
Holger <53127691+munich-ml@users.noreply.github.com>
2020-02-25 12:07:40 +0100

    Update MLPy_helper_funcs.py
    plot_prediction_examples added

--- MLPy_helper_funcs.py
+++ MLPy_helper_funcs.py
@@ -53,3 +53,36 @@ def plot_confusion_matrix(cm, xticks, yticks, normalize=False, ignore_main_diago
                     color = "white" if val > thresh else "black")
                     
     plt.colorbar()
+    
+    
+    
+def plot_prediction_examples(test_class, class_names, y_pred, y_test, X_test, 
+                             n_cols=10):
+    
+    print("Evaluating examples of test_class={}, '{}'".format(test_class, 
+                                                              class_names[test_class]))
+    
+    # step 1: seperating right and wrong examples
+    pred_right = []
+    pred_wrong = []
+    for i, (val_test, val_pred) in enumerate(zip(y_pred, y_test)):
+        if val_test == test_class:
+            if val_test == val_pred:
+                pred_right.append((i, val_pred))
+            else:
+                pred_wrong.append((i, val_pred))
+    
+    print("Classified right: {} images".format(len(pred_right)))
+    print("Classified wrong: {} images".format(len(pred_wrong)))
+    
+    # step 2: plotting random examples of right and wrong predictions
+    plt.figure(figsize=(n_cols*1.3, 4))
+    for row, predictions in enumerate([pred_right, pred_wrong]):
+        for col, idx in enumerate(np.random.randint(0, len(predictions), n_cols)):
+            i, pred_val = predictions[idx]
+            plt.subplot(2, n_cols, n_cols*row+col+1)
+            plt.imshow(X_test[i], cmap="binary")
+            plt.axis('off')
+            plt.title(class_names[pred_val], fontsize=12)
+    plt.tight_layout()
+    plt.show()
